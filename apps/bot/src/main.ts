39389aa0736cc9bb6da0a6bd89ec8e7d5f2bab04
Thomas Kortyka <info@toxictoast.de>
2021-07-10 23:22:15 +0200

    delete controller and service, add config, connect to microservice

--- apps/bot/src/main.ts
+++ apps/bot/src/main.ts
@@ -7,14 +7,35 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const configService = app.get(ConfigService);
+  //
+  const port = configService.get<number>('APP_PORT', 3333);
+  const user = configService.get<string>('RABBITMQ_USER');
+  const password = configService.get<string>('RABBITMQ_PASSWORD');
+  const host = configService.get<string>('RABBITMQ_HOST');
+  const queueName = configService.get<string>('RABBITMQ_QUEUE_NAME');
+  //
+  app.enableShutdownHooks();
+  //
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${user}:${password}@${host}`],
+      queue: queueName,
+      queueOptions: {
+        durable: true,
+      },
+    }
+  });
+  app.startAllMicroservices();
+  //
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log('Listening at http://localhost:' + port);
   });
 }
 
